3042c00da19c03fadd9a1963b47011b07cffcafa
shubhamswarnkar0611 <iamshubham0611@gmail.com>
2024-01-24 19:05:58 +0530

    Created the /add-product route and When the user clicks on submit parse both the input tags and show it in console

--- Server.js
+++ Server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.use((req, res, next) => {
-    // res.send("<h1>hello i am express.js</h1>")
-    console.log("hello i am middleware");
-    next();
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use("/add-product",(req, res, next) => {
+    
+    res.send("<form method='POST' action='/product'><h5>Product</h5><input type='text' name='title'/></br><h5>Size</h5><input type='number' name='size'/><button type='submit'>Add Product</button></form>");
 });
 
-app.use((req, res, next) => {
-    console.log("hello i am another middleware");
-    res.send("<h1>hello i am another middleware</h1>");
+app.post("/product",(req, res, next) => {
+    console.log(req.body)
+    res.redirect('/add-product');
+});
+
+app.use("/",(req, res, next) => {
+    res.send("<h1>hello i am express.js</h1>")
+    // console.log("hello i am middleware");
 });
 
 app.listen(3000);
+
