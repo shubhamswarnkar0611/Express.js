4e3dbfd0fa99180b6ef4d047e34a9c7c292be032
shubhamswarnkar0611 <iamshubham0611@gmail.com>
2024-01-27 20:31:47 +0530

    Added Dynamic Routes for Product Detail

---
+++ 00-starting-setup/controllers/shop.js
@@ -0,0 +1,59 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
+
+exports.getProductId=(req,res,next)=>{
+  const productId = req.params.ProductId;
+  Product.findProductById(productId,product =>{
+    res.render('shop/product-detail',{
+      prod: product,
+      pageTitle: 'Product Detail',
+      path: `/products`
+
+    })
+  })
+  
+
+
+  // res.redirect("/");
+  
+  }
---
+++ 00-starting-setup/routes/shop.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/product', shopController.getProducts);
+
+router.get('/product/:ProductId', shopController.getProductId);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
