cd00f130cf33e2223a1ac365f773870bab8cbf59
shubhamswarnkar0611 <iamshubham0611@gmail.com>
2024-02-02 16:19:43 +0530

    Adding Product to a Cart and Removing Product From Cart

--- Project-Shop-with-cart/app.js
+++ Project-Shop-with-cart/app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -35,10 +37,14 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart)
+Cart.belongsTo(User)
+Cart.belongsToMany(Product,{through:CartItem})
+Product.belongsToMany(Cart,{through:CartItem})
 
 sequelize
-  // .sync({ force: true })
-  .sync()
+  .sync({ force: false })
+  // .sync()
   .then(result => {
     return User.findByPk(1);
     // console.log(result);
@@ -51,6 +57,10 @@ sequelize
   })
   .then(user => {
     // console.log(user);
+    return user.createCart();
+    
+  })
+  .then(result => {
     app.listen(3000);
   })
   .catch(err => {
